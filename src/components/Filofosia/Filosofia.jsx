0b2d2ab75531780cd8e2a6a9aa6d93bc29baa80f
Raulmoyano2970 <raulmoyano2970@gmail.com>
2023-04-27 16:48:59 -0300

    arreglo

--- src/components/Filofosia/Filosofia.jsx
+++ src/components/Filofosia/Filosofia.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Imagen1 from '../../Assets/valorant2.png'
 import "../Filofosia/Filosofia.css";
+import {motion} from "framer-motion"
+import {fadeIn} from "../../variants"
 const Filosofia = () => {
   return (
     <div className="Filosofia" id="filosofia">
       <div className='filosofia1'>
-        <div className='parrafo1'>
+        <motion.div 
+             variants={fadeIn("right", 0.3)}
+             initial="hidden"
+             whileInView={"show"}
+             viewport={{once: false, amount: 0.3
+             }}
+        
+        className='parrafo1'>
         <h2 className="titulo_presentacion">Quienes Somos</h2>
         <p>Nuestra empresa se dedica a la enseñanza y entrenamiento en el videojuego Valorant,
              uno de los juegos más populares en el mundo de los deportes electrónicos (esports).
@@ -15,8 +24,9 @@ const Filosofia = () => {
            las áreas de Valorant. Desde la mecánica del juego y el uso de las diferentes armas y 
            habilidades de los personajes, hasta la estrategia y el trabajo en equipo, nuestras clases
             están diseñadas para ayudar a los jugadores a mejorar en todas las áreas.    </p>
-    </div>
-      <div className='contenedor__imagen '>
+    </motion.div>
+      <div   
+      className='contenedor__imagen '>
             <img src={Imagen1} className='imagen1' alt="" />
         </div>
       </div>
